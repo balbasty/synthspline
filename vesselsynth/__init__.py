40de195a3870d750fb992cc966c1bb1705ab37ab
EtienneChollet <etiennechollet2626@gmail.com>
2023-10-24 10:07:17 -0400

    Changed the way some things are imported to increase compatibility. Changed location of _Backend to utils.py

--- vesselsynth/__init__.py
+++ vesselsynth/__init__.py
@@ -1,23 +1,7 @@
 import interpol
 
-from .brent import *
+from .brent import Brent
 from .save_exp import *
 from .io import *
 from .synth import *
 from .utils import *
-
-class _Backend:
-
-    def __init__(self) -> None:
-        self._jitfields = interpol.backend.jitfields
-
-    @property
-    def jitfields(self):
-        return self._jitfields
-
-    @jitfields.setter
-    def jitfields(self, value):
-        self._jitfields = value
-        interpol.backend.jitfields = value
-
-backend = _Backend()
